f77eced5420ff065e08ddf61f257a71da34a01f0
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-01 19:05:19 +0300

    ДЗ №2. Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte b = 120;
+        short s = -2000;
+        int i = 1234567;
+        long l = -98765434232l;
+        float f = 12.21f;
+        double d = 65432.5432;
+        char c = 'C';
+        boolean bool = s < i;
+        System.out.println(b + "\n" +  s + "\n" + i + "\n" + l + "\n" + f + "\n" + d + "\n" + c + "\n" + bool);
     }
 }
