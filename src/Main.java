65d8bc6edfe3a74bbe5c8296067d327ebb79736d
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-01 19:19:11 +0300

    ДЗ №2. Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        //Задание №1
         byte b = 120;
         short s = -2000;
         int i = 1234567;
@@ -10,5 +12,13 @@ public class Main {
         char c = 'C';
         boolean bool = s < i;
         System.out.println(b + "\n" +  s + "\n" + i + "\n" + l + "\n" + f + "\n" + d + "\n" + c + "\n" + bool);
+
+        //Задание №2
+        float WeightFirst = 78.2f;
+        float WeightSecond = 82.7f;
+        float TotalWeight = WeightFirst + WeightSecond;
+        System.out.println("\nОбщий вес двух боксеров: " + TotalWeight);
+        float DifferenceBetweenWeights = WeightSecond - WeightFirst;
+        System.out.println("Разница между весами двух боксеров: " + DifferenceBetweenWeights);
     }
 }
